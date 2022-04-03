49f568d5ce5a1d2d88b484240cb5414c24e8d963
Folarin Odeyemi <folarin.odeyemi@gmail.com>
2022-04-03 21:07:45 +0100

    third commit

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -13,7 +13,7 @@ export const Default = Template.bind({});
 Default.args = {
     task: {
         id: '1',
-        title: 'Test Task 1',
+        title: 'Test Task',
         state: 'TASK_INBOX',
         updatedAt: new Date(2021, 0, 1, 9, 0),
     },
